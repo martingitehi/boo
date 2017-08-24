7813a67bfc5de2347cce890a06151b207ae7f13f
Martin Gitehi <martingitehi@live.com>
2017-08-24 11:13:31 +0300

    initial commit

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 module.exports = {
-   database: 'mongodb://192.168.137.1:27017/boo',
-   //database: 'mongodb://tini:thebigbadwolf2017@ds139072.mlab.com:39072/kapu',
+   database: 'mongodb://tini:bigbadwolf2017@ds157233.mlab.com:57233/boo',
     secret: 'heybooboo'
 };
