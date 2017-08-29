20d228bbddb4c6497bdd92718a22021fe32818a7
Martin Gitehi <martingitehi@live.com>
2017-08-29 18:37:57 +0300

    edits to https server

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ mongoose.connection.on('open', function (err) {
 var app = express();
 //view engine setup
 
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
